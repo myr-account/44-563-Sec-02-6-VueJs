fe1e0b8486306ee5fc20ea6106ebf0596db99085
bomma.anil <anil.bommaa@gmail.com>
2019-10-27 14:44:50 -0500

    - updated index.html file - updated app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,26 @@ new Vue({
     el: '#app',
     data: {
         // data varibles goes here
-        toggleBtn: false
+        toggleBtn: false,
+        userDetails: {
+            category: 'Choose...',
+            gender: '',
+            sid: ''
+        },
     },
     computed: {
         // computed properties goes here
     },
     methods: {
         // methods goes here
-
         togglePreview: function () {
             this.toggleBtn ? this.toggleBtn = false : this.toggleBtn = true;
+        },
 
+        geneeateID: function () {
+            let id = parseInt(Math.random() * 1000000);
+            id = "919" + id.toString();
+            this.userDetails.sid = id;
         }
-
     }
 })
